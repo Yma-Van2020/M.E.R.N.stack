da483551279958d7beca1d7a8666cdc3013797c2
Yucong Ma <candicema2018@gmail.com>
2021-08-31 14:43:41 -0700

    finished

--- React/prop_it_up/src/components/MyNewComponent.js
+++ React/prop_it_up/src/components/MyNewComponent.js
@@ -1,30 +1,45 @@
 import React, { Component } from "react"
 
-class PersonCard extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            personage: props.age
-        }
-    }
+//Class components
 
-    increaseAge = () => {
-                this.setState({
-                    personage: this.state.personage + 1
-                })
-            }
+// class PersonCard extends Component{
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             personage: props.age
+//         }
+//     }
 
-     render() {
-        const {firstName, lastName, hairColor} = this.props;
+//     increaseAge = () => {
+//                 this.setState({
+//                     personage: this.state.personage + 1
+//                 })
+//             }
+
+//      render() {
+//         const {firstName, lastName, hairColor} = this.props;
         
-        return (
-            <div>
-                <h2>{firstName} {lastName} </h2>
-                <p>{this.state.personage}</p>
-                <p>{hairColor}</p>
-                <button onClick={this.increaseAge}>Birthday button for {firstName} {lastName}</button>
-            </div>
-        );
-    }
+//         return (
+//             <div>
+//                 <h2>{firstName} {lastName} </h2>
+//                 <p>{this.state.personage}</p>
+//                 <p>{hairColor}</p>
+//                 <button onClick={this.increaseAge}>Birthday button for {firstName} {lastName}</button>
+//             </div>
+//         );
+//     }
+// }
+
+//functional components
+const PersonCard = props =>{
+    return(
+        <div>
+            <h1>{ props.lastName }, { props.firstName }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    )
 }
+
+
 export default PersonCard;
