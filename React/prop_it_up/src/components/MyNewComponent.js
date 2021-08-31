718421ccf360bb57d33387adfb119ef3965fb5c0
Yucong Ma <candicema2018@gmail.com>
2021-08-30 18:06:55 -0700

    finished

---
+++ React/prop_it_up/src/components/MyNewComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class PersonCard extends Component{
+    render(){
+        return(
+            <div>
+                <h2>{this.props.firstName}, {this.props.lastName}</h2>
+                <p>Age: {this.props.age}</p>
+                <p>Hair Color: {this.props.hairColor}</p>
+            </div>
+        );
+    }
+}
+
+export default PersonCard;
