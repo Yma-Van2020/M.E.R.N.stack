8c0b0d1f8c5a4f6ed30a8be83460c106dcc0f3d8
Yucong Ma <candicema2018@gmail.com>
2021-09-04 15:14:09 -0700

    finished

---
+++ React/pokemon_api/src/components/Display.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+
+const Display = () => {
+    const [pokemon, setPokemon] = useState(0)
+
+    useEffect(() => {
+        fetch("https://pokeapi.co/api/v2/pokemon")
+        .then(response => response.json())
+        .then(response => setPokemon({
+            pokemons: response.results,
+            isClicked: false
+        }))
+    }, [])
+
+    const clickHandler = () => {
+        setPokemon({
+            ...pokemon,
+            isClicked: !pokemon.isClicked
+        })
+    }
+
+    return (
+        <div>
+
+            <button className="btn btn-warning m-5" onClick={clickHandler} >Fetch Pokemon</button>
+            {pokemon.isClicked
+            && pokemon.pokemons.map((p,i) => {
+                return(<div key={i}>{p.name}</div>)
+            })
+            }
+            
+        </div>
+    )
+}
+
+export default Display
