6fb000d99c456320918fd658a7bfc98eab62fb6d
Yucong Ma <candicema2018@gmail.com>
2021-09-04 15:45:54 -0700

    finished

--- React/axios_pokemon_api/src/components/Display.jsx
+++ React/axios_pokemon_api/src/components/Display.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import axios from "axios"
 
 const Display = () => {
     const [pokemon, setPokemon] = useState({})
 
     useEffect(() => {
-        fetch("https://pokeapi.co/api/v2/pokemon")
-        .then(response => response.json())
+        axios.get("https://pokeapi.co/api/v2/pokemon")
         .then(response => setPokemon({
-            pokemons: response.results,
+            pokemons: response.data.results,
             isClicked: false
         }))
     }, [])
@@ -17,13 +17,14 @@ const Display = () => {
             ...pokemon,
             isClicked: !pokemon.isClicked
         })
+        console.log(pokemon)
     }
 
     return (
         <div>
 
             <button className="btn btn-warning m-5" onClick={clickHandler} >Fetch Pokemon</button>
-            {pokemon.isClicked
+            {pokemon.isClicked && pokemon.pokemons !== undefined
             && pokemon.pokemons.map((p,i) => {
                 return(<div key={i}>{p.name}</div>)
             })
