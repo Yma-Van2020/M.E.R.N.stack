fcf591c676364a882a84d03666bcd2ddd2960dd4
Yucong Ma <candicema2018@gmail.com>
2021-09-09 12:13:19 -0700

    fix minor bug

--- MongoDB/Jokes/server/routes/jokes.routes.js
+++ MongoDB/Jokes/server/routes/jokes.routes.js
@@ -1,7 +1,7 @@
 const JokeController = require("../controllers/jokes.controller");
 
 module.exports = app => {
-  app.get("/api/jokes/", JokeController.findAlljokes);
+  app.get("/api/jokes/", JokeController.findAllJokes);
   app.get("/api/jokes/:id", JokeController.findOneSingleJoke);
   app.put("/api/jokes/update/:id", JokeController.updateExistingJoke);
   app.post("/api/jokes/new", JokeController.createNewJoke);
