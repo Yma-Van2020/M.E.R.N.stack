cebdb9f92190933bd8e17342e26bc49b33b95fc0
Yucong Ma <candicema2018@gmail.com>
2021-09-09 12:24:09 -0700

    add timestamp

--- MongoDB/Jokes/server/models/jokes.model.js
+++ MongoDB/Jokes/server/models/jokes.model.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 
 const JokeSchema = new mongoose.Schema({
-	setup: String,
-	punchline: String
-});
+	setup: {
+		type:String,
+		required: [true, "setup is required"],
+		minlength: [10, "setup should have at least 10 chars"]
+	},
+	punchline: {
+		type:String,
+		require:[true, "punchline is required"],
+		minlength: [3, 'punchline should have at least 3 chars']
+	}
+}, {timestamps: true});
 
 const Joke = mongoose.model("Joke", JokeSchema);
 
