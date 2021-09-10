37f184867c2ef580901d89e5b4459b3fb5d78c80
Yucong Ma <candicema2018@gmail.com>
2021-09-09 17:43:24 -0700

    finished

--- Full stack MERN/HelloWorld/server.js
+++ Full stack MERN/HelloWorld/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const cors = require('cors') // This is new
 const app = express();
-const port = 8000;
-
-require("./server/routes/person.routes.js")(app);
-
-app.listen(port, () => console.log(`Listening on port: ${port}`) );
+app.use(cors()) // This is new
+require('./server/routes/person.routes')(app);
+app.listen(8000, () => {
+    console.log("Listening at Port 8000")
+})
