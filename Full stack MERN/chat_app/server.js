d3eb00db3488cf3bab34c017db18d8e8d7395aed
Yucong Ma <candicema2018@gmail.com>
2021-09-15 14:02:05 -0700

    turn msg into an obj

--- Full stack MERN/chat_app/server.js
+++ Full stack MERN/chat_app/server.js
@@ -11,23 +11,22 @@ const io = socketio(server)
 //SET STATI FOLDER
 app.use(express.static(path.join(__dirname, "public")))
 
-const botName = "Chatroom-bot29438:D"
+const botName = "Chatroom-Bot29438:D"
 io.on("connection", socket => {
     //welcome current user
     socket.emit("message", formatMessages(botName, "welcome to chatroom:)"))
 
     //when connects
-    socket.broadcast.emit("message", formatMessages(botName,"A user has joined the chatroom");
+    socket.broadcast.emit("message", formatMessages(botName,"A user has joined the chatroom"))
 
     //when disconnects
     socket.on("disconnect", () => {
         io.emit("message", formatMessages(botName,"A use has left the chatroom")
-    })
+    )})
 
     //listen for chatmessages
     socket.on("chatMessage", msg => {
-        io.emit("message", formatMessages("User", msg)
-    })
+        io.emit("message", formatMessages("User", msg))})
 })
 const PORT = 3000 || process.env.PORT
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
