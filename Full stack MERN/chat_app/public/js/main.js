2f1e631d35656cd7b186531ec9607d8405b1ed7c
Yucong Ma <candicema2018@gmail.com>
2021-09-15 13:32:02 -0700

    update

--- Full stack MERN/chat_app/public/js/main.js
+++ Full stack MERN/chat_app/public/js/main.js
@@ -2,14 +2,30 @@
  
  const socket = io();
 
+ //msg from server
  socket.on("message", message => {
-     console.log(message)
+     outputMessage(message)
  })
 
  //msg submit
  chatForm.addEventListener("submit", e => {
      e.preventDefault()
+
+     //get msg text
      const msg = e.target.elements.msg.value
 
-     console.log(msg)
+     //emit msg to server
+     socket.emit("chatMessage", msg)
  })
+
+ function outputMessage(msg) {
+     const div = document.createElement("div")
+     div.classList.add("message")
+     div.innerHTML = `
+     <p class="meta">Brad <span>9:12pm</span></p>
+						<p class="text">
+							${msg}
+						</p>
+     `;
+     document.querySelector(".chat-messages").appendChild(div);
+ }
