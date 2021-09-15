d35f55fcde7b30ea81936ceafbc2329a03f0aa93
Yucong Ma <candicema2018@gmail.com>
2021-09-15 13:53:00 -0700

    add botname

--- Full stack MERN/chat_app/server.js
+++ Full stack MERN/chat_app/server.js
@@ -3,7 +3,7 @@ const http = require("http")
 const express = require("express");
 
 const socketio = require("socket.io")
-
+const formatMessages = require("./utils/messages")
 const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
@@ -11,21 +11,22 @@ const io = socketio(server)
 //SET STATI FOLDER
 app.use(express.static(path.join(__dirname, "public")))
 
+const botName = "Chatroom-bot29438:D"
 io.on("connection", socket => {
     //welcome current user
-    socket.emit("message", "welcome to chatroom:)")
+    socket.emit("message", formatMessages(botName, "welcome to chatroom:)"))
 
     //when connects
-    socket.broadcast.emit("message", "A user has joined the chatroom");
+    socket.broadcast.emit("message", formatMessages(botName,"A user has joined the chatroom");
 
     //when disconnects
     socket.on("disconnect", () => {
-        io.emit("message", "A use has left the chatroom")
+        io.emit("message", formatMessages(botName,"A use has left the chatroom")
     })
 
     //listen for chatmessages
     socket.on("chatMessage", msg => {
-        io.emit("message", msg)
+        io.emit("message", formatMessages("User", msg)
     })
 })
 const PORT = 3000 || process.env.PORT
