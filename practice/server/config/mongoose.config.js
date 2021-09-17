c393abe3f28c112eecdf3de28aa7da8231db997b
Katelyn Ma <74885386+Yma-Van2020@users.noreply.github.com>
2021-09-17 13:35:52 -0700

    Delete practice directory

--- practice/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb://localhost/piratesdb", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => console.log("Database connection establish"))
-    .catch(err => console.log("there was an error", err))
