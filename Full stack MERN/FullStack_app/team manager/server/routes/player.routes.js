1fb584a2d18634310e8b7ac47ae504ac5187be20
Yucong Ma <candicema2018@gmail.com>
2021-09-21 13:45:43 -0700

    update

--- Full stack MERN/FullStack_app/team manager/server/routes/player.routes.js
+++
@@ -1,9 +0,0 @@
-const PlayerController = require("../controllers/player.controller")
-
-module.exports = (app) =>{
-    app.post("/api/players", PlayerController.createPlayer)
-    app.get("/api/players", PlayerController.getAll)
-    app.get("/api/players/:id", PlayerController.getOne)
-    app.delete("/api/players/:id", PlayerController.deletePlayer)
-    app.put("/api/players/:id", PlayerController.updateplayer)
-}
